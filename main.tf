c2056a264a37d6dc93f16f903c56a0714c099ec0
Mitchell <mitchellryansmith@gmail.com>
2022-10-27 01:19:37 -0500

    added ecr manager role

--- main.tf
+++ main.tf
@@ -23,3 +23,47 @@ resource "aws_ecr_repository" "ecr_repo2" {
       Environment = var.aws_environment
   }
 }
+
+### Creation of IAM Role ECR ###
+
+resource "aws_iam_role" "ecr-manager" {
+  name = "ecr_manager"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "ecr-policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["ecr:CompleteLayerUpload",
+                      "ecr:GetAuthorizationToken",
+                      "ecr:UploadLayerPart",
+                      "ecr:InitiateLayerUpload",
+                      "ecr:BatchCheckLayerAvailability",
+                      "ecr:PutImage"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.aws_environment
+  }
+}
