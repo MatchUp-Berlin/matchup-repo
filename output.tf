14f158e28a315f6d0fe33e7fd9b6a49832d887cd
Mitchell <mitchellryansmith@gmail.com>
2022-10-27 18:57:11 -0500

    updated output

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "ecr_repo_name" {
 output "ecr_repo_url" {
   value = aws_ecr_repository.ecr_repo.repository_url
 }
+
+output "ecr_repo_name2" {
+  value = aws_ecr_repository.ecr_repo2.name
+}
+
+output "ecr_repo_url2" {
+  value = aws_ecr_repository.ecr_repo2.repository_url
+}
